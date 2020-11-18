d5cfffff30bdd7e71bbda4cffb362b0f29d427dd
s1l3ntq <qimiller89@gmail.com>
2020-11-17 23:11:52 -0600

    commit tree 95824de2d00660ffd106a086623126c55fbac79c

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,47 @@
+def sort_array_asc(ascend)
+  ascend.sort do |a, b|
+    a <=> b
+  end
+end
 
+def sort_array_desc(descend)
+    descend.sort { |x, y| y <=> x }
+end
+
+def sort_array_char_count(characters)
+    characters.sort { |l, d| l.length <=> d.length }
+
+end
+
+def swap_elements(swap)
+    swap.replace(["blake", "scott", "ashley"])
+end
+
+def reverse_array(int)
+    int.reverse 
+end
+
+def kesha_maker(mula)
+    mula.each do |dolla|
+        dolla[2] = "$"
+    end
+end
+
+def find_a(letter)
+    letter.find_all { |word| word[0] == "a" }
+    
+end
+
+def sum_array(add)
+    add.inject(:+)
+end
+
+def add_s(sim)
+    sim.collect do |city|
+        if sim[1] == city
+            city
+        else
+            city + "s"
+        end
+    end
+end
